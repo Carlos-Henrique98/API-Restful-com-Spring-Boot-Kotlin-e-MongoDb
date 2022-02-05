c2320339a04edb97be7df247695330fca218c5c0
Carlos-Henrique98 <ichfs9108@gmail.com>
2022-02-04 23:51:28 -0300

    project created

---
+++ Ponto-Inteligente/src/main/kotlin/com/smart/point/PontoInteligenteApplication.kt
@@ -0,0 +1,11 @@
+package com.smart.point
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontoInteligenteApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontoInteligenteApplication>(*args)
+}
---
+++ Ponto-Inteligente/src/test/kotlin/com/smart/point/PontoInteligenteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smart.point
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
